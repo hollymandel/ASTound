4217513ebc523ab8103bb4003b1281f2312da386
hollymandel <holly.mandel@berkeley.edu>
2024-06-02 09:54:02 -0700

    black

--- astound/smartnode.py
+++ astound/smartnode.py
@@ -1,16 +1,21 @@
 import ast
-import astor
-import jedi
 import logging
 
-jedi.settings.fast_parser = False # otherwise jedi will only maintain one script object! 
+import astor
+import jedi
+
+jedi.settings.fast_parser = (
+    False  # otherwise jedi will only maintain one script object!
+)
+
 
 class ForbiddenNodeType(BaseException):
     pass
 
+
 class Source:
-    """ mutable wrapper around source text """
-    
+    """mutable wrapper around source text"""
+
     def __init__(self, name, path):
         self._name = name
         with open(path, "r") as file:
@@ -23,12 +28,15 @@ class Source:
     def name(self):
         return self._name
 
+
 class Node:
-    """ thin wrapper around AST for formatting and printing. Lots of special casing to
-    convert python grammar into human- (and claude-) readable text """ 
-    
-    def __init__(self, ast_node = None):
-        if any(isinstance(ast_node, forbidden_type) for forbidden_type in FORBIDDEN_TYPES):
+    """thin wrapper around AST for formatting and printing. Lots of special casing to
+    convert python grammar into human- (and claude-) readable text"""
+
+    def __init__(self, ast_node=None):
+        if any(
+            isinstance(ast_node, forbidden_type) for forbidden_type in FORBIDDEN_TYPES
+        ):
             raise ForbiddenNodeType("import nodes excluded")
         if isinstance(ast_node, ast.Expr):
             ast_node = ast_node.value
@@ -45,32 +53,30 @@ class Node:
             return f"Parent Module"
         if isinstance(self._ast_node, ast.Assign):
             return (
-                "Assignment: (" + 
-                ", ".join([Node(target).__repr__() for target in self._ast_node.targets]) +
-                ") = (" +
-                Node(self._ast_node.value).__repr__() +
-                ")"
+                "Assignment: ("
+                + ", ".join(
+                    [Node(target).__repr__() for target in self._ast_node.targets]
+                )
+                + ") = ("
+                + Node(self._ast_node.value).__repr__()
+                + ")"
             )
         if isinstance(self._ast_node, ast.Call):
             return (
-                "Function Call: (" +
-                Node(self._ast_node.func).__repr__() + 
-                ") with args (" +
-                ", ".join([Node(arg).__repr__() for arg in self._ast_node.args]) +
-                ")"
+                "Function Call: ("
+                + Node(self._ast_node.func).__repr__()
+                + ") with args ("
+                + ", ".join([Node(arg).__repr__() for arg in self._ast_node.args])
+                + ")"
             )
         if isinstance(self._ast_node, ast.BoolOp):
             return (
-                "Boolean Op: (" +
-                ", ".join([Node(value).__repr__() for value in self._ast_node.values]) +
-                ")"
+                "Boolean Op: ("
+                + ", ".join([Node(value).__repr__() for value in self._ast_node.values])
+                + ")"
             )
         if isinstance(self._ast_node, ast.If):
-            return (
-                "If (" +
-                self._ast_node.test.__repr__() +
-                ")"
-            )
+            return "If (" + self._ast_node.test.__repr__() + ")"
 
         # not all Ast types have a name, but all should have some sort of identifying data
         try:
@@ -84,34 +90,34 @@ class Node:
                 raise ValueError from AttributeError
 
         return (
-            f"{pretty_type(type(self._ast_node))} '{get_name}' " +
-            f"at {(self._ast_node.lineno, self._ast_node.col_offset)}"
+            f"{pretty_type(type(self._ast_node))} '{get_name}' "
+            + f"at {(self._ast_node.lineno, self._ast_node.col_offset)}"
         )
 
-    
     def assign_text(self, text):
         self._text = text
 
     def text(self):
         return self._text
-        
+
     def print_children(self):
         for subnode in self._ast_node.body:
             try:
                 print(Node(subnode).__repr__())
             except ForbiddenNodeType:
                 continue
- 
+
+
 class SmartNode(Node):
-    """ methods for recursive summarization """ 
-    
-    def __init__(self, ast_node = None, source: Source = None, parent = None):
+    """methods for recursive summarization"""
+
+    def __init__(self, ast_node=None, source: Source = None, parent=None):
         super().__init__(ast_node)
-        self._source = source 
+        self._source = source
         self._parent = parent
         self._children = {}
         self._summary = ""
-        
+
         # to my knowledge, neither ast nor jedi is good at tracking down class inheritance,
         # so here we keep track of any base classes. This comes up if you see a call to "super()".
         # That being said, this functionality is limited to simple inheritance structures, because
@@ -123,22 +129,23 @@ class SmartNode(Node):
             return self._summary
         else:
             return super().__repr__()
-            
+
     def infer_inheritance(self):
-        if not isinstance(self._ast_node, ast.ClassDef): 
+        if not isinstance(self._ast_node, ast.ClassDef):
             if self._parent:
                 return self._parent.infer_inheritance()
             else:
                 return None
         if len(self._ast_node.bases) > 1:
-            logging.warning("ASTound cannot resolve multiple inheritance. Defaulting to first parent class.")
+            logging.warning(
+                "ASTound cannot resolve multiple inheritance. Defaulting to first parent class."
+            )
         return astor.to_source(self._ast_node.bases[0]).split("\n")[0]
 
- 
     def get_text(self):
         if isinstance(self._ast_node, ast.Module):
             return self._source.text()
-            
+
         if isinstance(self._ast_node, ast.Call):
             # assert False
             line, col = self._ast_node.lineno, self._ast_node.col_offset
@@ -146,12 +153,12 @@ class SmartNode(Node):
 
             if get_first_ref.full_name.__contains__("super"):
                 return (
-                    "Function call '" + 
-                    astor.to_source(self._ast_node) +  
-                    "' inherited from " + 
-                    self._inheritance
+                    "Function call '"
+                    + astor.to_source(self._ast_node)
+                    + "' inherited from "
+                    + self._inheritance
                 )
-                
+
             function_name = get_first_ref.full_name.split(".")[-1]
 
             visitor = ExtractFunctionSource(function_name, self._source)
@@ -159,9 +166,9 @@ class SmartNode(Node):
             return visitor.function_def
 
         return self._source.text().split("\n")[
-            self._ast_node.lineno-1:self._ast_node.end_lineno
+            self._ast_node.lineno - 1 : self._ast_node.end_lineno
         ]
-    
+
     def get_child_by_loc(self, line, col):
         child_list = []
         if hasattr(self._ast_node, "body"):
@@ -169,10 +176,10 @@ class SmartNode(Node):
         if hasattr(self._ast_node, "targets"):
             child_list += self._ast_node.targets
         if hasattr(self._ast_node, "value"):
-            child_list += [ self._ast_node.value ]
+            child_list += [self._ast_node.value]
         if hasattr(self._ast_node, "values"):
             child_list += self._ast_node.values
-        
+
         for subnode in child_list:
             if subnode.lineno == line and subnode.col_offset == col:
                 return subnode
@@ -182,58 +189,66 @@ class SmartNode(Node):
         if (line, col) in self._children:
             raise ValueError("child already exists")
 
-        self._children[(line, col)] = SmartNode(self.get_child_by_loc(line, col), source = self._source, parent = self)
+        self._children[(line, col)] = SmartNode(
+            self.get_child_by_loc(line, col), source=self._source, parent=self
+        )
 
     def link_child(self, line, col):
         parent_type = str(type(self._ast_node))
         child_type = str(type(self._children[line, col]))
         link_field = self._children[line, col]._link_field
 
-        # TODO - CACHE LINKS! 
-        
+        # TODO - CACHE LINKS!
+
         link_prompt = (
-            "Please describe in one sentence the relationship between a python syntax parent node of type " + 
-            str(type(self._ast_node)) + 
-            " and a child node of type " + 
-            str(type(self._children[line, col])) + 
-            f" from the {from_field} field of the parent."
+            "Please describe in one sentence the relationship between a python syntax parent node of type "
+            + str(type(self._ast_node))
+            + " and a child node of type "
+            + str(type(self._children[line, col]))
+            + f" from the {from_field} field of the parent."
         )
 
     def attach_manual(self, name, child):
         self._children[name] = child
-        
+
     def summarize(self, client):
         if len(self._summary) > 0:
             return self._summary
 
-        individual_prompt = (
-            INDIVIDUAL_SUMMARY_HEADER + 
-            "".join(self.get_text())
-        )
+        individual_prompt = INDIVIDUAL_SUMMARY_HEADER + "".join(self.get_text())
 
-        individual_summary = client.messages.create(
-            **CLAUDE_KWARGS, 
-            messages=[
-                {"role": "user", "content": individual_prompt}
-            ]
-        ).content[0].text
+        individual_summary = (
+            client.messages.create(
+                **CLAUDE_KWARGS,
+                messages=[{"role": "user", "content": individual_prompt}],
+            )
+            .content[0]
+            .text
+        )
 
         if len(self._children) == 0:
             self.summary = individual_summary
         else:
             joint_prompt = (
-                JOINT_SUMMARY_HEADER + 
-                individual_summary + 
-                "\n".join([f"{child_header(x)}{x.summarize(client)}" for x in self._children.values()])
+                JOINT_SUMMARY_HEADER
+                + individual_summary
+                + "\n".join(
+                    [
+                        f"{child_header(x)}{x.summarize(client)}"
+                        for x in self._children.values()
+                    ]
+                )
             )
-    
-            joint_summary = client.messages.create(
-                **CLAUDE_KWARGS, 
-                messages=[
-                    {"role": "user", "content": joint_prompt}
-                ]
-            ).content[0].text
-            
+
+            joint_summary = (
+                client.messages.create(
+                    **CLAUDE_KWARGS,
+                    messages=[{"role": "user", "content": joint_prompt}],
+                )
+                .content[0]
+                .text
+            )
+
             self._summary = joint_summary
-            
+
         return self._summary
--- astound/summarize.py
+++ astound/summarize.py
@@ -1,56 +1,66 @@
-import anthropic
 import ast
-import smartnode 
 from types import MappingProxyType
 from typing import Optional
 
-MESSAGE_KWARGS = MappingProxyType({
-    "model": "claude-3-haiku-20240307",
-    "max_tokens": 200,
-    "temperature": 0.0,
-    "system": SYSTEM_PROMPT,
-    # "api_key": YOUR_API_KEY
-})
+import anthropic
+import smartnode
+
+MESSAGE_KWARGS = MappingProxyType(
+    {
+        "model": "claude-3-haiku-20240307",
+        "max_tokens": 200,
+        "temperature": 0.0,
+        "system": SYSTEM_PROMPT,
+        # "api_key": YOUR_API_KEY
+    }
+)
 
 FORBIDDEN_TYPES = (ast.ImportFrom, ast.Import, ast.ListComp)
 
 with open("prompts.json", "r") as f:
     PROMPTS = MappingProxyType(json.load(f))
 
-def summarize(node: SmartNode, client, prompts: Optional[Union[MappingProxyType, Dict]] = None):
+
+def summarize(
+    node: SmartNode, client, prompts: Optional[Union[MappingProxyType, Dict]] = None
+):
     prompts = prompts or PROMPTS
-    
+
     if len(node._summary) > 0:
         return node._summary
 
-    individual_prompt = (
-        prompts["individual_header"] + 
-        "".join(node.get_text())
-    )
+    individual_prompt = prompts["individual_header"] + "".join(node.get_text())
 
-    individual_summary = client.messages.create(
-        **MESSAGE_KWARGS, 
-        messages=[
-            {"role": "user", "content": individual_prompt}
-        ]
-    ).content[0].text
+    individual_summary = (
+        client.messages.create(
+            **MESSAGE_KWARGS, messages=[{"role": "user", "content": individual_prompt}]
+        )
+        .content[0]
+        .text
+    )
 
     if len(node._children) == 0:
         node.summary = individual_summary
     else:
         joint_prompt = (
-            prompts["joint_header"]  + 
-            individual_summary + 
-            "\n".join([f"{child_header(x)}{x.summarize(client)}" for x in node._children.values()])
+            prompts["joint_header"]
+            + individual_summary
+            + "\n".join(
+                [
+                    f"{child_header(x)}{x.summarize(client)}"
+                    for x in node._children.values()
+                ]
+            )
         )
 
-        joint_summary = client.messages.create(
-            **MESSAGE_KWARGS, 
-            messages=[
-                {"role": "user", "content": joint_prompt}
-            ]
-        ).content[0].text
-        
+        joint_summary = (
+            client.messages.create(
+                **MESSAGE_KWARGS, messages=[{"role": "user", "content": joint_prompt}]
+            )
+            .content[0]
+            .text
+        )
+
         node._summary = joint_summary
-        
+
     return node._summary
